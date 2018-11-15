f3b600400ea772a86beab0cb5815eee040765bd0
Wonjoon Goo <hi.wonjoon@gmail.com>
2018-11-14 18:10:45 -0600

    mujoco pretrained models

--- learner/README.md
+++ learner/README.md
@@ -6,11 +6,24 @@ git submodule update --init --recursive
 
 You should see the contents of `baselines`
 
-## Generate Trajectory and Videos
+## Train
 
 ```
-./run.py --env_id BreakoutNoFrameskip-v4 --model_path ./models/breakout/checkpoints/03600 --record_video
+OPENAI_LOG_FORMAT='stdout,log,csv,tensorboard' OPENAI_LOGDIR=/home/wonjoon/workspace/LfL_new/learner/models/log/reacher python -m baselines.run --alg=ppo2 --env=Reacher-v2 --save_interval=20
 ```
+
+## Generate Trajectory and Videos
+
+First, download the pretrained models [link](), and extract under the `models` directory.
+
+```
+./run_test.py --env_id BreakoutNoFrameskip-v4 --env_type atari --model_path ./models/breakout/checkpoints/03600 --record_video
+```
+or,
+```
+./run_test.py --env_id Reacher-v2 --env_type mujoco --model_path ./models/reacher/checkpoints/01000 --record_video
+```
+
 
 Replace the arguments as you want. Currently models for each 100 learning steps (upto 3600 learning steps) are uploaded.
 
--- learner/run_test.py
+++ learner/run_test.py
@@ -5,10 +5,11 @@ import gym
 from gym import wrappers, logger
 
 sys.path.append('./baselines/')
-import baselines.ppo2.ppo2 as ppo2
+from baselines.ppo2.model import Model
 from baselines.common.policies import build_policy
 from baselines.common.cmd_util import make_vec_env
 from baselines.common.vec_env.vec_frame_stack import VecFrameStack
+from baselines.common.vec_env.vec_normalize import VecNormalize
 from baselines.common.vec_env.vec_video_recorder import VecVideoRecorder
 
 class RandomAgent(object):
@@ -20,17 +21,25 @@ class RandomAgent(object):
         return self.action_space.sample()
 
 class PPO2Agent(object):
-    def __init__(self, env, path):
+    def __init__(self, env, env_type, path):
         ob_space = env.observation_space
         ac_space = env.action_space
 
-        policy = build_policy(env,'cnn')
-        make_model = lambda : ppo2.Model(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=1, nbatch_train=1,
+        if env_type == 'atari':
+            policy = build_policy(env,'cnn')
+        elif env_type == 'mujoco':
+            policy = build_policy(env,'mlp')
+
+        make_model = lambda : Model(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=1, nbatch_train=1,
                         nsteps=1, ent_coef=0., vf_coef=0.,
                         max_grad_norm=0.)
         self.model = make_model()
         if path:
             self.model.load(path)
+            try:
+                env.load(path)
+            except AttributeError:
+                pass
 
     def act(self, observation, reward, done):
         a,v,state,neglogp = self.model.step(observation)
@@ -39,6 +48,7 @@ class PPO2Agent(object):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('--env_id', default='', help='Select the environment to run')
+    parser.add_argument('--env_type', default='', help='mujoco or atari')
     parser.add_argument('--model_path', default='')
     parser.add_argument('--episode_count', default=100)
     parser.add_argument('--record_video', action='store_true')
@@ -51,16 +61,22 @@ if __name__ == '__main__':
     #env = gym.make(args.env_id)
 
     #env id, env type, num envs, and seed
-    env = make_vec_env(args.env_id, 'atari', 1, 0,
+    env = make_vec_env(args.env_id, args.env_type, 1, 0,
                        wrapper_kwargs={
                            'clip_rewards':False,
                            'episode_life':False,
                        })
     if args.record_video:
         env = VecVideoRecorder(env,'.',lambda steps: True, 10000) # Always record every episode
-    env = VecFrameStack(env, 4)
 
-    agent = PPO2Agent(env,args.model_path)
+    if args.env_type == 'atari':
+        env = VecFrameStack(env, 4)
+    elif args.env_type == 'mujoco':
+        env = VecNormalize(env,ob=True,ret=False,eval=True)
+    else:
+        assert False, 'not supported env type'
+
+    agent = PPO2Agent(env,args.env_type,args.model_path)
     #agent = RandomAgent(env.action_space)
 
     episode_count = args.episode_count
