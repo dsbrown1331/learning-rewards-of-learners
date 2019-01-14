b758d445f0fe2e6183829729779d3543ff83b976
dsbrown1331 <dsbrown1331@gmail.com>
2019-01-14 13:02:39 -0600

    non-sorted demos

---
+++ scripts/create_tacc_command_files_raw.py
@@ -0,0 +1,15 @@
+def create_command_file(envname):
+    command_str = "module load tacc-singularity\n"
+    command_str += 'singularity exec --nv ${SINGULARITY_CACHEDIR}/tacc-maverick-ml-latest.simg python -c "import torch; print(torch.cuda.is_available())"\n'
+    command_str += "singularity exec --nv ${SINGULARITY_CACHEDIR}/tacc-maverick-ml-latest.simg python LearnAtariNoviceSnippets.py --env_name=" + envname +" --seed=0 --reward_model_path=$WORK/output/learned_rewards/" + envname + "_12_raw_pref.params\n"
+    print(command_str)
+    f = open("commands_raw_" + envname,'w')
+    f.write(command_str)
+    f.close()
+    
+envs = ['mspacman', 'videopinball', 'hero', 'beamrider', 'qbert', 'seaquest', 'breakout', 'spaceinvaders', 'pong', 'enduro' ]
+for e in envs:
+    print("+"*20)
+    print(e)
+    print("+"*20)
+    create_command_file(e)
